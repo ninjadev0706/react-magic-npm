b12b0262b77634ff53a6af6123b652ad3a4c3566
ninjadev0706 <103828922+ninjadev0706@users.noreply.github.com>
2022-07-26 17:54:13 +0200

    initialized the project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { DataProvider, ThemeProvider } from './context';
+
+import { HomePage } from './pages/HomePage';
+
+import './index.scss';
+import { Navbar } from './components/Navbar';
+
+export const App: FC = (): JSX.Element =>
+  <Router>
+    <Navbar />
+    <Routing />
+  </Router>;
+
+const Routing: FC = (): JSX.Element =>
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+  </Routes>;
